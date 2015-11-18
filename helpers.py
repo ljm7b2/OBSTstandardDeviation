022841345d73dac4796da678b711a80e97f0b9f2
Luke McDuff <ljm7b2@mail.umkc.edu>
2015-11-18 01:45:36 -0600

    added std_deviation

--- helpers.py
+++ helpers.py
@@ -1,3 +1,5 @@
+from math import sqrt
+from functools import lru_cache
 
 def get_optimal_roots(root, c, i, j):
     return [rt[0] for rt in root if rt[-1] == c[i][j]]
@@ -28,19 +30,91 @@ def my_sum(freq, i, j):
     return s
 
 
-def build_trees(i, j, trees, my_dict, level, size):
+
+def build_trees(i, j, trees, my_dict, level, size, recheck):
     for key in my_dict[(i, j)]:
-        # trees.append(str(key["root"]) + " L:(" + str(level) + ") -- ")
         trees.append([level, key["root"]])
         level += 1
         if key["left"] != -1:
-            build_trees(key["left"][0], key["left"][-1], trees, my_dict, level, size)
+            build_trees(key["left"][0], key["left"][-1], trees, my_dict, level, size, recheck)
         if key["right"] != -1:
-            build_trees(key["right"][0], key["right"][-1], trees, my_dict, level, size)
-            # trees.append("break2")
+            build_trees(key["right"][0], key["right"][-1], trees, my_dict, level, size, recheck)
             level -= 1
-            if(len(my_dict[(i, j)]) > 1) and (i, j) != (1, 5):
+            if(len(my_dict[(i, j)]) > 1) and (i, j) != (1, size):
                 my_dict[(i, j)].pop(0)
+                recheck[0] += 1
 
+
+def get_std_deviation(all_trees, total_sum, n_max):
+    temp_sum1, temp_sum2, sum_list, counter = 0, 0, [], 1
+    for t in all_trees:
+        for node in t:
+            temp_sum1 += node[0] * (node[-1] / total_sum)
+            temp_sum2 += (node[0] ** 2) * (node[-1] / total_sum)
+            if counter == n_max:
+                sum_list.append((temp_sum1, temp_sum2))
+                temp_sum1, temp_sum2, counter = 0, 0, 0
+            counter += 1
+    return [sqrt(val[-1] - (val[0] ** 2)) for val in sum_list]
+
+
+def compute_w_c_r(p, n_max, C, W, R, S, node_dict):
+
+    # construct weight matrix
+    for i in range(n_max):
+        W[i][i] = p[i]
+        for j in range(i + 1, n_max):
+            W[i][j] = W[i][j-1] + p[j]
+    # construct cost, root matrix
+    for i in range(n_max):
+        C[i][i] = W[i][i]
+        R[i][i] = i + 1
+        node_dict[(i+1, i+1)] = [{"root": i+1, "left": -1, "right": -1}]
+    for L in range(2, n_max + 1):
+        progress(L)
+        for i in range(0, n_max - L + 1):
+            j = i + L - 1
+            C[i][j] = 1000000000
+            roots = []
+            for r in range(i, j + 1):                                            # this is slow makes algorithm n^4
+                c = (C[i][r - 1] if r > i else 0) + (C[r + 1][j] if r < j else 0) + (my_sum(p, i, j))
+                if c < C[i][j]:
+                    C[i][j] = c
+                    R[i][j] = r + 1
+            # get all optimal roots and build root data dictionary (map)
+                roots.append((r + 1, c))
+            loc, sub_dict = build_dict(i+1, j+1, [rt[0] for rt in roots if rt[-1] == C[i][j]])
+            node_dict[loc] = sub_dict
+
+
+def organize_trees(all_trees, trees, temp, count, n_max):
+    for tr in trees:
+        count += 1
+        temp.append(tr)
+        if count == n_max - 1:
+            all_trees.append(sorted(temp))
+            temp, count = [], -1
+    return all_trees
+
+def progress(n):
+
+    if n == 100:
+        print("10%")
+    elif n == 200:
+        print("20%")
+    elif n == 300:
+        print("30%")
+    elif n == 400:
+        print("40%")
+    elif n == 500:
+        print("50%")
+    elif n == 600:
+        print("60%")
+    elif n == 700:
+        print("70%")
+    elif n == 800:
+        print("80%")
+    elif n == 900:
+        print("90%")
 
 
--- main3.py
+++ main3.py
@@ -1,96 +1,65 @@
 from helpers import *
+import time
+import random
+from timeit import default_timer as timer
+start = timer()
 # define Global Variables
 
-p = [26, 20, 26, 19, 26, 21, 23, 25, 28, 17, 26, 26, 29, 21, 23]
-# p = [6, 4, 2, 6, 4]
-# p = [16, 18, 16, 16, 22, 20, 14, 19, 14, 23, 17, 23, 15, 17]
+# p = [26, 20, 26, 19, 26, 21, 23, 25, 28, 17, 26, 26, 29, 21, 23]
+p = [6, 4, 2, 6, 4]
+#p = [16, 18, 16, 16, 22, 20, 14, 19, 14, 23, 17, 23, 15, 17]
 # p = [19, 19, 20, 21, 22, 18, 21, 18, 19, 18, 22, 18, 18]
+#p = [4 for i in range(250)]
+# p = [10, 3, 9, 2, 0, 10]
+#p = [3, 3, 4, 4]
+# p = [3, 2, 4, 1]
+
+
+
+
 
 n_max = len(p)
-
+total_sum = sum(p)
 q = [0 for i in range(n_max)]
 C = [[0]*n_max for i in range(n_max)]
 W = [[0]*n_max for i in range(n_max)]
 R = [[0]*n_max for i in range(n_max)]
-
-# sum_dict = sum1(p, n_max)
-
-node_dict = {}
-
-
-def compute_w_c_r():
-    global p, n_max, C, W, R, node_dict
-
-    # construct weight matrix
-    for i in range(n_max):
-        W[i][i] = p[i]
-        for j in range(i + 1, n_max):
-            W[i][j] = W[i][j-1] + p[j]
-
-    # construct cost, root matrix
-    for i in range(n_max):
-        C[i][i] = W[i][i]
-        R[i][i] = i + 1
-        node_dict[(i+1, i+1)] = [{"root": i+1, "left": -1, "right": -1}]
-
-    for L in range(2, n_max + 1):
-        for i in range(0, n_max - L + 1):
-            j = i + L - 1
-
-            C[i][j] = 1000000000
-            roots = []
-            for r in range(i, j + 1):
-                                                                                    # this is slow makes algorithm n^4
-                c = (C[i][r - 1] if r > i else 0) + (C[r + 1][j] if r < j else 0) + (my_sum(p, i, j))
-
-                if c < C[i][j]:
-                    C[i][j] = c
-                    R[i][j] = r + 1
-
-            # get all optimal roots and build root data dictionary (map)
-                roots.append((r + 1, c))
-            loc, sub_dict = build_dict(i+1, j+1, [rt[0] for rt in roots if rt[-1] == C[i][j]])
-            node_dict[loc] = sub_dict
-
-
-
+S = [[0]*n_max for i in range(n_max)]
+node_dict, count, trees, recheck, ct = {}, 0, [], [1], 0
+all_trees = []
 # main
 
-compute_w_c_r()
+print("Computing tables")
+compute_w_c_r(p, n_max, C, W, R, S, node_dict)
 
-count = 0
-trees = []
-stack = []
 
-build_trees(1, n_max, trees, node_dict, 1, n_max)
+while ct < recheck[0]:
+    build_trees(1, n_max, trees, node_dict, 1, n_max, recheck)
+    ct += 1
+
+all_trees = organize_trees(all_trees, trees, [], -1, n_max)
+
+print(all_trees)
+print(len(all_trees))
+print(get_std_deviation(all_trees, total_sum, n_max))
 
 print(W)
 print(C)
 print(R)
 print()
-# all_trees.sort()
-
-all_trees, temp, count = [], [], -1
-for tr in trees:
-    count += 1
-    temp.append(tr)
-    if count == n_max - 1:
-        all_trees.append(sorted(temp))
-        temp, count = [], -1
-
-print(all_trees)
 
 
-print()
-print()
 
-for i in range(1, n_max + 1):
-    for t in range(i, n_max+ 1):
-        if len(node_dict[(i,t)]) > 1:
-            print("(", end="")
-            for s in node_dict[(i, t)]:
-                print("{0:2d}".format(s["root"]), end=" ")
-            print(")", end="")
-        print("{0:2d}".format(node_dict[(i,t)][0]["root"]), end=" ")
-    print()
+# for i in range(1, n_max + 1):
+#     for t in range(i, n_max+ 1):
+#         if len(node_dict[(i,t)]) > 1:
+#             print("(", end="")
+#             for s in node_dict[(i, t)]:
+#                 print("{0:2d}".format(s["root"]), end=" ")
+#             print(")", end="")
+#         print("{0:2d}".format(node_dict[(i, t)][0]["root"]), end=" ")
+#     print()
 
+end = timer()
+print("Time elapsed:", end - start, "seconds")
+print("Time elapsed:", (end - start)/ 60, "minutes")
